62cc8f759954b9a55f2b270109b3501e2375db57
rimet08 <rmetzner@proton.me>
2025-10-29 12:44:51 +0100

    int

--- main.c
+++ main.c
@@ -3,3 +3,22 @@
 #include <time.h>
 #include <stdbool.h>
 #include <unistd.h>
+
+int main() {
+    // DIGITAL CLOCK //
+
+    time_t rawtime = 0;
+    struct tm *pTime = NULL;
+    bool isRunning = true;
+
+    printf("DIGITAL CLOCK\n");
+
+    while (isRunning) {
+
+        isR
+    }
+
+
+
+
+}
